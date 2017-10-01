2b7352456c0464e89aa8170dd65588a39ff4053f
Francis Li <mail@francisli.com>
2017-10-01 15:20:26 -0700

    Add a maven pom file for building. Add .gitignore to not add compiled binaries. Add support for SHIFT key modifier for snapping to 90 degree horizontal/vertical movements.

--- src/deadpixel/keystone/Keystone.java
+++ src/deadpixel/keystone/Keystone.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) 2009-15 David Bouchard
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,16 +23,16 @@ import processing.core.*;
 import processing.data.XML;
 import processing.event.MouseEvent;
 import processing.opengl.PGraphics3D;
- 
+
 /**
  * This class manages the creation and calibration of keystoned surfaces.
- * 
- * To move and warp surfaces, place the Keystone object in calibrate mode. It catches mouse events and 
+ *
+ * To move and warp surfaces, place the Keystone object in calibrate mode. It catches mouse events and
  * allows you to drag surfaces and control points with the mouse.
  *
- * The Keystone object also provides load/save functionality, once you've calibrated the layout to 
- * your liking. 
- * 
+ * The Keystone object also provides load/save functionality, once you've calibrated the layout to
+ * your liking.
+ *
  * Version: 0.31
  */
 public class Keystone {
@@ -61,19 +61,19 @@ public class Keystone {
 		dragged = null;
 
 		// check the renderer type
-		// issue a warning if we're not in 3D mode 
+		// issue a warning if we're not in 3D mode
 		PGraphics pg = parent.g;
 		if ((pg instanceof PGraphics3D) == false ) {
 			PApplet.println("The keystone library will not work with 2D graphics as the renderer because it relies on texture mapping. " +
 					"Try P3D or OPENGL.");
 		}
-		
+
 		PApplet.println("Keystone " + VERSION);
 	}
 
 	/**
-	 * Creates and registers a new corner pin keystone surface. 
-	 * 
+	 * Creates and registers a new corner pin keystone surface.
+	 *
 	 * @param w width
 	 * @param h height
 	 * @param res resolution (number of tiles per axis)
@@ -86,40 +86,40 @@ public class Keystone {
 	}
 
 	/**
-	 * Starts the calibration mode. Mouse events will be intercepted to drag surfaces 
+	 * Starts the calibration mode. Mouse events will be intercepted to drag surfaces
 	 * and move control points around.
 	 */
 	public void startCalibration() {
 		calibrate = true;
 	}
-	
+
 	/**
 	 * Stops the calibration mode
 	 */
 	public void stopCalibration() {
 		calibrate = false;
 	}
-	
+
 	/**
 	 * Toggles the calibration mode
 	 */
 	public void toggleCalibration() {
 		calibrate = !calibrate;
 	}
-	
+
 	public boolean isCalibrating() {
 		return calibrate;
 	}
 
 	/**
 	 * Returns the version of the library.
-	 * 
+	 *
 	 * @return String
 	 */
 	public String version() {
 		return VERSION;
 	}
-	
+
 	/**
 	 * Saves the layout to an XML file.
 	 */
@@ -150,21 +150,21 @@ public class Keystone {
 				}
 			}
 			root.addChild(surface);
-			
+
 		}
 		/*
 		// write the settings to keystone.xml in the sketch's data folder
 		try {
 			OutputStream stream = parent.createOutput(parent.dataPath(filename));
-			root.save(stream); 
+			root.save(stream);
 		} catch (Exception e) {
 			PApplet.println(e.getStackTrace());
-		}		
+		}
 		*/
 		parent.saveXML(root, filename);
 		PApplet.println("Keystone: layout saved to " + filename);
 	}
-	
+
 	/**
 	 * Saves the current layout into "keystone.xml"
 	 */
@@ -177,7 +177,7 @@ public class Keystone {
 	 */
 	public void load(String filename) {
 		XML root = parent.loadXML(filename);
-		
+
 		/*
 		// Guy's version -- need to figure out why this doesn't work
 		surfaces.clear();
@@ -190,7 +190,7 @@ public class Keystone {
 			surface.load(surfaceEl);
 		}
 		*/
-		
+
 		XML[] surfaceXML = root.getChildren("surface");
 		for (int i=0; i < surfaceXML.length; i++) {
 			surfaces.get(i).load(surfaceXML[i]);
@@ -198,20 +198,22 @@ public class Keystone {
 
 		PApplet.println("Keystone: layout loaded from " + filename);
 	}
-	
+
 	/**
 	 * Loads a saved layout from "keystone.xml"
 	 */
 	public void load() {
 		load("keystone.xml");
 	}
-	
+
+
+	int startX, startY;
 
 	/**
 	 * @invisible
 	 */
 	public void mouseEvent(MouseEvent e) {
-		
+
 		// ignore input events if the calibrate flag is not set
 		if (!calibrate)
 			return;
@@ -223,11 +225,19 @@ public class Keystone {
 
 		case MouseEvent.PRESS:
 			CornerPinSurface top = null;
-			// navigate the list backwards, as to select 
+			// navigate the list backwards, as to select
 			for (int i=surfaces.size()-1; i >= 0; i--) {
 				CornerPinSurface s = surfaces.get(i);
 				dragged = s.select(x, y);
 				if (dragged != null) {
+					if (dragged instanceof MeshPoint) {
+						MeshPoint pt = (MeshPoint) dragged;
+						startX = (int) (pt.x + pt.parent.x);
+						startY = (int) (pt.y + pt.parent.y);
+					} else {
+						startX = x;
+						startY = y;
+					}
 					top = s;
 					break;
 				}
@@ -236,8 +246,8 @@ public class Keystone {
 			if (top != null) {
 				// moved the dragged surface to the beginning of the list
 				// this actually breaks the load/save order.
-				// in the new version, add IDs to surfaces so we can just 
-				// re-load in the right order (or create a separate list 
+				// in the new version, add IDs to surfaces so we can just
+				// re-load in the right order (or create a separate list
 				// for selection/rendering)
 				//int i = surfaces.indexOf(top);
 				//surfaces.remove(i);
@@ -246,8 +256,16 @@ public class Keystone {
 			break;
 
 		case MouseEvent.DRAG:
-			if (dragged != null)
+			if (dragged != null) {
+				if (e.isShiftDown()) {
+					if (Math.abs(x - startX) > Math.abs(y - startY)) {
+						y = startY;
+					} else {
+						x = startX;
+					}
+				}
 				dragged.moveTo(x, y);
+			}
 			break;
 
 		case MouseEvent.RELEASE:
